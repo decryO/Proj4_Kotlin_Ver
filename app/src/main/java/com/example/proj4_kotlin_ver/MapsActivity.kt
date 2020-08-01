408b4a1729e49e9dfdbf2587e303d64e4fe1fc1f
decryO <lesagvjctbment@gmail.com>
2020-08-01 17:35:35 +0900

    Fuelを用いてのHTTP通信を行うクラスを追加 それに伴うgradleの変更 ダイアログフラグメントを追加

--- app/src/main/java/com/example/proj4_kotlin_ver/MapsActivity.kt
+++ app/src/main/java/com/example/proj4_kotlin_ver/MapsActivity.kt
@@ -9,8 +9,11 @@ import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -21,13 +24,26 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.*
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListener {
 
     private lateinit var mMap: GoogleMap
     // デフォルトの座標(京都)
     private var latLng = LatLng(34.985458, 135.7577551)
     private var alertRadius: Double = 0.0
+
+    // HeartRails様API URL
+    // 都道府県一覧
+    private val getToDoHuURL: String = "https://express.heartrails.com/api/json?method=getPrefectures"
+    private var selectedTodohu: String = ""
+    // 路線一覧
+    private val getLineURL: String = "https://express.heartrails.com/api/json?method=getLines&prefecture="
+    private var selectedLine: String = ""
+    // 駅一覧
+    private val getStationURL: String = "https://express.heartrails.com/api/json?method=getStations&line="
+
     private lateinit var channelID: String
+    private lateinit var dialog: DialogFragment
+    private lateinit var httpAccessor: HttpAccessor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,51 +52,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapView.onCreate(savedInstanceState)
         mapView.onResume()
         mapView.getMapAsync(this)
+        dialog = DialogFragment()
+        httpAccessor = HttpAccessor()
+
+        // 単調になるので下に切り分け
+        selectToDoHu.setOnClickListener(this)
+        selectLine.setOnClickListener(this)
+        selectStation.setOnClickListener(this)
+        bStart.setOnClickListener(this)
 
         channelID = getString(R.string.notify_channel_id)
         sliderText.text = "アラートラインのサイズ : " + (alertRadius / 1)
-
-        bStart.setOnClickListener{
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                // 通知のタイトル
-                val name = getString(R.string.notify_name)
-
-                // 通知の説明
-                val descriptionText = getString(R.string.notify_description)
-
-                // 通知の重要度 ここでは通知バーに表示されるが音は出ない設定(IMPORTANCE_LOW)
-                val importance = NotificationManager.IMPORTANCE_LOW
-
-                val mChannel = NotificationChannel(channelID, name, importance)
-                mChannel.apply {
-                    description = descriptionText
-                }
-
-                val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-                notificationManager.createNotificationChannel(mChannel)
-
-                val notify = NotificationCompat
-                    .Builder(this, channelID)
-                    .apply {
-                        setSmallIcon(R.drawable.ic_notify)
-                        setContentText(descriptionText)
-                        setContentTitle(name)
-                    }.build()
-                notificationManager.notify(1, notify)
-            }
-        }
-
-        selectToDoHu.setOnClickListener{
-            ToDoHuText.text = "都道府県選択！！"
-        }
-
-        selectLine.setOnClickListener{
-            lineText.text = "路線選択！！"
-        }
-
-        selectStation.setOnClickListener {
-            stationText.text = "駅選択！！"
-        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -106,6 +88,63 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 .fillColor(0x220000FF)
                 .strokeWidth(5F)
             )
+        }
+    }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        when(id) {
+            R.id.bStart -> setAlarmButtonSelected()
+            R.id.selectToDoHu -> tlButtonSelected(getToDoHuURL)
+            R.id.selectLine -> tlButtonSelected(getLineURL + selectedTodohu)
+            R.id.selectStation -> dialog.show(supportFragmentManager, "simple")
+        }
+    }
+
+    private fun tlButtonSelected(url: String) {
+        println("start")
+        ToDoHuText.text = String(httpAccessor.getJson(url))
+        println("end")
+//        url.httpGet().response { request, response, result ->
+//            when(result) {
+//                is Result.Success -> {
+//                    ToDoHuText.text = String(response.data)
+//
+//                }
+//                is Result.Failure -> {
+//                    ToDoHuText.text = "失敗しました"
+//                }
+//            }
+//        }
+    }
+
+    private fun setAlarmButtonSelected() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // 通知のタイトル
+            val name = getString(R.string.notify_name)
+
+            // 通知の説明
+            val descriptionText = getString(R.string.notify_description)
+
+            // 通知の重要度 ここでは通知バーに表示されるが音は出ない設定(IMPORTANCE_LOW)
+            val importance = NotificationManager.IMPORTANCE_LOW
+
+            val mChannel = NotificationChannel(channelID, name, importance)
+            mChannel.apply {
+                description = descriptionText
+            }
+
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(mChannel)
+
+            val notify = NotificationCompat
+                .Builder(this, channelID)
+                .apply {
+                    setSmallIcon(R.drawable.ic_notify)
+                    setContentText(descriptionText)
+                    setContentTitle(name)
+                }.build()
+            notificationManager.notify(1, notify)
         }
     }
 }
